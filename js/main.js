bb28c64a7ff4994459edc245561007e8cad84227
Pasha <pashaabramchuk92@gmail.com>
2020-07-12 12:21:48 +0300

    Design.dot 2018

---
+++ js/main.js
@@ -0,0 +1,145 @@
+$(document).ready(function() {
+    
+    //scrolling
+    
+    $('.gallery__btn, .footer__btn, .menu__link, .top__logo, .mobile__link').click(function (e) {
+        e.preventDefault();
+        elementClick = $(this).attr('href');
+        destination = $(elementClick).offset().top;
+        $('body,html').animate({scrollTop: destination}, 800)
+    });
+    
+    
+    //Menu-open hamburger
+    
+    $('.menu-open').click(function () {
+        $('.menu-collapse').toggleClass('d-none').css('order', '1');
+        $('.mobile__list').toggleClass('menu-opened');
+    });
+    
+    $('.mobile__link').click(function () {
+        $('.menu-collapse').addClass('d-none');
+    });
+    
+    //opened
+    
+    $('.write').click(function() {
+        $('.opened').removeClass('d-none');
+        $('.write-close').toggleClass('d-none');
+    });
+    
+    $('.write-close').click(function() {
+        $('.opened').addClass('d-none');
+        $('.write-close').addClass('d-none');
+    });
+    
+    
+    
+    
+    //Передача инфо о кнопке в модальное окно
+     $('button.form__btn').click(function() {
+        var parent = $(this).attr('data-parent');
+        var modal = $(this).attr('data-target')
+        $(modal).find('input[name=target]').val(parent);
+    })
+    
+    //Валидация и отправка формы
+    $('[data-submit]').on('click', function(e) {
+        e.preventDefault();
+        $(this).parent('form').submit();
+    })
+    $.validator.addMethod(
+        "regex",
+        function(value, element, regexp) {
+            var re = new RegExp(regexp);
+            return this.optional(element) || re.test(value);
+        },
+        "Please check your input."
+    );
+
+    // Функция валидации и вывода сообщений
+    function valEl(el) {
+
+        el.validate({
+            rules: {
+                tel: {
+                    required: true,
+                    regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+                },
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                tel: {
+                    required: 'Поле обязательно для заполнения',
+                    regex: 'Телефон может содержать символы + - ()'
+                },
+                name: {
+                    required: 'Поле обязательно для заполнения',
+                },
+                email: {
+                    required: 'Поле обязательно для заполнения',
+                    email: 'Неверный формат E-mail'
+                }
+            },
+
+            // Начинаем проверку id="" формы
+            submitHandler: function(form) {
+                $('#loader').fadeIn();
+                var $form = $(form);
+                var $formId = $(form).attr('id');
+                switch ($formId) {
+                    // Если у формы id="goToNewPage" - делаем:
+                    case 'goToNewPage':
+                        $.ajax({
+                                type: 'POST',
+                                url: $form.attr('action'),
+                                data: $form.serialize(),
+                            })
+                            .always(function(response) {
+                                //ссылка на страницу "спасибо" - редирект
+                                location.href = '';
+                                //отправка целей в Я.Метрику и Google Analytics
+                                ga('send', 'event', 'masterklass7', 'register');
+                                yaCounter27714603.reachGoal('lm17lead');
+                            });
+                        break;
+                    // Если у формы id="popupResult" - делаем:
+                    case 'popupResult':
+                        $.ajax({
+                                type: 'POST',
+                                url: $form.attr('action'),
+                                data: $form.serialize(),
+                            })
+                            .always(function(response) {
+                                setTimeout(function() {
+                                    $('#loader').fadeOut();
+                                }, 800);
+                                setTimeout(function() {
+                                    $('#overlay').fadeIn();
+                                    $form.trigger('reset');
+                                    //строки для остлеживания целей в Я.Метрике и Google Analytics
+                                }, 1100);
+                                $('#overlay').on('click', function(e) {
+                                    $(this).fadeOut();
+                                });
+
+                            });
+                        break;
+                }
+                return false;
+            }
+        })
+    }
+
+    // Запускаем механизм валидации форм, если у них есть класс .js-form
+    $('.js-form').each(function() {
+        valEl($(this));
+    });
+    
+});
